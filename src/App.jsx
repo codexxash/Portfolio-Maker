9eaf2fa19dfc479eba0e5595ce530b918a72a6b1
Ashutosh <ashusighrajput88@gmail.com>
2026-04-08 15:22:28 +0530

    fix the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import { FaTrash } from "react-icons/fa";
 
 const App = () => {
 
   // Basic Info State
   const [formData, setFormData] = useState({
-    name: '',
-    about: '',
-    contact: ''
+    name: 'John Doe',
+    about: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus eveniet molestias maiores suscipit laborum? Obcaecati qui, labore quibusdam quod sint autem incidunt laborum vitae consequatur laboriosam a, beatae delectus enim?',
+    contact: '123456789',
+    institution: 'Oxford University',
+    field: 'Masters of Business Administration',
+    mail: 'johndoe@gmail.com',
+    address: 'EI classico 14th street New York',
+    role: 'Buisness Analyst'
+
   })
+
 
   // Skills State
   const [skills, setSkills] = useState([])
@@ -19,6 +27,24 @@ const App = () => {
     title: '',
     description: ''
   })
+
+  // Experience State
+
+  const [experience, setExperience] = useState([])
+  const [experienceInput, setExperienceInput] = useState({
+    company: '',
+    description: ''
+  })
+
+  const [photo, setPhoto] = useState(null)
+
+  // Photo Handler
+
+  const handlePhoto = (e) => {
+    const file = e.target.files[0]
+    setPhoto(URL.createObjectURL(file))
+
+  }
 
   // ───────────────────────────────
   // Basic Info Handler
@@ -50,36 +76,89 @@ const App = () => {
   }
 
   const addProject = () => {
-    if (projectInput.title.trim() === '') return // blank project mat add karo
+    if (projectInput.title.trim() === '') return
     setProjects([...projects, projectInput])
-    setProjectInput({ title: '', description: '' }) // input clear karo
+    setProjectInput({ title: '', description: '' })
   }
 
   const removeProject = (index) => {
     setProjects(projects.filter((_, i) => i !== index))
   }
 
+  // Experience Handler
+
+  const handleExperienceInput = (e) => {
+    const { name, value } = e.target
+    setExperienceInput({ ...experienceInput, [name]: value })
+
+  }
+
+  const addExperience = () => {
+    if (experienceInput.company.trim() === '') return
+    setExperience([...experience, experienceInput])
+    setExperienceInput({ company: '', description: '' })
+  }
+
+  const removeExperience = (index) => {
+    setExperience(experience.filter((_, i) => i !== index))
+  }
+
+
   return (
-    <div className='flex justify-around pt-10 bg-black min-h-screen text-white gap-10 px-10'>
+    <div className='flex flex-wrap  bg-black max-h-auto text-white'>
 
       {/* ───── FORM SIDE ───── */}
-      <form className="flex flex-col gap-4 w-1/2">
-        <h2 className='text-2xl font-bold'>Portfolio Generator 🔥</h2>
+      <form className="flex flex-col gap-4 w-1/4 px-4 bg-gray-900  pt-10 ">
+        {/* <h2 className='text-2xl font-bold'>Portfolio Generator 🔥</h2> */}
 
-        {/* Basic Info */}
+        {/* Photo */}
+
+        <input
+          className='py-3 px-1 max-w-17 rounded text-xs bg-gray-400 cursor-pointer outline-none'
+          type="file"
+          accept='image/*'
+          onChange={handlePhoto}
+          placeholder='choose one'
+        />
+
         <input
           name='name'
           onChange={handleChange}
           value={formData.name}
-          className='border rounded p-2 bg-transparent'
+          className='border-b rounded p-2 bg-transparent outline-none'
           type="text"
           placeholder='Enter Your Name' />
+
+        <input
+          name='mail'
+          onChange={handleChange}
+          className='border-b rounded p-2 bg-transparent outline-none'
+          value={formData.mail}
+          placeholder='Enter your mail'
+          type="text" />
+
+        <input
+          name='address'
+          onChange={handleChange}
+          className='border-b rounded p-2 bg-transparent outline-none'
+          placeholder='Address'
+          value={formData.address}
+          type="text" />
+
+        <input
+          name='role'
+          onChange={handleChange}
+          value={formData.role}
+          placeholder='Role/Position'
+          className='border-b rounded p-2 bg-transparent outline-none'
+          type="text" />
+
 
         <input
           name='about'
           onChange={handleChange}
           value={formData.about}
-          className='border rounded p-2 bg-transparent'
+          className='border-b rounded p-2 bg-transparent outline-none'
           type="text"
           placeholder='About Yourself' />
 
@@ -87,9 +166,29 @@ const App = () => {
           name='contact'
           onChange={handleChange}
           value={formData.contact}
-          className='border rounded p-2 bg-transparent'
-          type="number"
+          className='border-b rounded p-2 bg-transparent outline-none'
+          type="tel"
           placeholder='Phone No' />
+
+        {/* Education */}
+        <input
+          name='institution'
+          onChange={handleChange}
+          value={formData.institution}
+          className='border-b rounded p-2 bg-transparent outline-none'
+          type="text"
+          placeholder='University/College Name' />
+
+
+        <input
+          name='field'
+          onChange={handleChange}
+          value={formData.field}
+          className='border-b rounded p-2 bg-transparent outline-none'
+          type="text"
+          placeholder='Field of study' />
+
+
 
         {/* Skills */}
         <hr className='border-gray-600' />
@@ -100,13 +199,13 @@ const App = () => {
             value={skillInput}
             onChange={(e) => setSkillInput(e.target.value)}
             type="text"
-            className='border rounded p-2 bg-transparent flex-1'
+            className='border-b rounded p-2 outline-none'
             placeholder='Add a skill (e.g. HTML)' />
           <button
             type='button'
             onClick={addSkill}
-            className='border rounded px-4 py-2'>
-            + Add
+            className=' rounded px-4 py-2 bg-blue-400 border-none cursor-pointer'>
+            Add
           </button>
         </div>
 
@@ -115,7 +214,7 @@ const App = () => {
           {skills.map((skill, index) => (
             <span key={index} className='border rounded px-2 py-1 flex items-center gap-1'>
               {skill}
-              <button type='button' onClick={() => removeSkill(index)}>❌</button>
+              <button type='button' onClick={() => removeSkill(index)}><FaTrash color='red' /></button>
             </span>
           ))}
         </div>
@@ -129,65 +228,146 @@ const App = () => {
           placeholder='Project Title'
           value={projectInput.title}
           onChange={handleProjectInput}
-          className='border rounded p-2 bg-transparent' />
+          className='border-b rounded p-2 bg-transparent outline-none' />
 
         <textarea
           name='description'
           placeholder='Project Description'
           value={projectInput.description}
           onChange={handleProjectInput}
-          className='border rounded p-2 bg-transparent' />
+          className='border-b rounded p-2 bg-transparent outline-none' />
 
         <button
           type='button'
           onClick={addProject}
-          className='border rounded px-4 py-2'>
-          + Add Project
+          className='bg-blue-400 rounded px-4 py-2'>
+           Add Project
         </button>
+
+        {/* Experience */}
+
+        <input
+          name='company'
+          placeholder='Company Name'
+          value={experienceInput.company}
+          onChange={handleExperienceInput}
+          className='border-b rounded p-2 bg-transparent outline-none' />
+
+        <textarea
+          name='description'
+          placeholder='Description'
+          value={experienceInput.description}
+          onChange={handleExperienceInput}
+          className='border-b rounded p-2 bg-transparent outline-none' />
+
+        <button
+          type='button'
+          onClick={addExperience}
+          className='bg-blue-400 rounded py-2'>
+           Add Experience
+        </button>
+
 
       </form>
 
+      
+
       {/* ───── PREVIEW SIDE ───── */}
-      <div className='w-1/2 border-l border-gray-600 pl-10'>
-        <h2 className='text-2xl font-bold'>Preview 👇</h2>
+      
+      <div className=' flex flex-col border-l w-252  border-gray-600 bg-[#0f0a1a] pt-6'>
+        {/* <h2 className='text-2xl font-bold'>Preview 👇</h2> */}
 
-        {/* Basic Info Preview */}
-        <h1 className='text-3xl font-bold mt-4'>{formData.name}</h1>
-        <p className='text-gray-300 mt-2'>{formData.about}</p>
-        <p className='text-gray-400 mt-1'>{formData.contact}</p>
+        <div className="hero flex flex-col items-center border-b ">
+          {/* Image Preview */}
 
-        {/* Skills Preview */}
-        {skills.length > 0 && (
-          <div className='mt-4'>
-            <h3 className='font-semibold text-lg'>Skills</h3>
-            <div className='flex flex-wrap gap-2 mt-2'>
-              {skills.map((skill, index) => (
-                <span key={index} className='border rounded px-2 py-1'>
-                  {skill}
-                </span>
-              ))}
-            </div>
+          {photo && (
+            <img
+              src={photo}
+              alt='Profile'
+              className='w-32 h-32 rounded-full object-cover ' />
+          )}
+          <h1 className='text-3xl font-semibold my-4'>{formData.name}</h1>
+          <span className=' px-3 py-2 rounded-lg text-white bg-purple-600'>{formData.role}</span>
+
+          <div className='flex justify-evenly w-full m-5'>
+            <span>{formData.mail}</span>
+            <span>{formData.address}</span>
+            <span className='text-gray-400'>{formData.contact}</span>
+
           </div>
-        )}
+        </div>
 
-        {/* Projects Preview */}
-        {projects.length > 0 && (
-          <div className='mt-4'>
-            <h3 className='font-semibold text-lg'>Projects</h3>
-            {projects.map((project, index) => (
-              <div key={index} className='border rounded p-3 mt-2'>
-                <h4 className='font-bold'>{project.title}</h4>
-                <p className='text-gray-300'>{project.description}</p>
-                <button
-                  type='button'
-                  onClick={() => removeProject(index)}
-                  className='text-red-400 text-sm mt-1'>
-                  Remove ❌
-                </button>
+        <div className="hero2 flex flex-row py-10">
+          <div className="section1 border-r px-5">
+            <h1 className='text-2xl font-bold'>About me</h1>
+            <p className='text-gray-500 mt-2 max-w-90 tracking-wide leading-tight '>{formData.about}</p>
+            <h1 className='text-2xl font-bold pt-10'>Education</h1>
+            <h4 className=' pt-4 text-lg'>{formData.institution}</h4>
+            <p className='text-gray-400'>{formData.field}</p>
+
+            {/* Experience preview */}
+
+            {experience.length > 0 && (
+              <div className='mt-4'>
+                <h3 className=' text-2xl font-bold pt-6'>Experience</h3>
+                {experience.map((experience, index) => (
+                  <div key={index} className=' p-3 mt-2'>
+                    <h4 className=' text-lg'>{experience.company}</h4>
+                    <p className='text-gray-500'>{experience.description}</p>
+                    <button
+                      type='button'
+                      onClick={() => removeExperience(index)}
+                      className='text-red-400 text-sm mt-1'>
+                      <FaTrash color='red' />
+                    </button>
+                  </div>
+                ))}
               </div>
-            ))}
+            )}
           </div>
-        )}
+          <div className="section2 flex flex-col px-7">
+
+            {/* Skills Preview */}
+
+
+            <h1 className='text-2xl font-bold '>Skills & Tools</h1>
+            {skills.length > 0 && (
+              <div className='mt-4'>
+                <h3 className='font-semibold text-lg'>Skills</h3>
+                <div className='flex flex-wrap gap-2 mt-2'>
+                  {skills.map((skill, index) => (
+                    <span key={index} className='bg-gray-600 rounded px-3 py-1'>
+                      {skill}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
+
+            {/* Project Preview */}
+
+            {projects.length > 0 && (
+              <div className='mt-4'>
+                <h3 className='font-semibold text-xl'>Projects</h3>
+                {projects.map((project, index) => (
+                  <div key={index} className=' p-3 mt-2'>
+                    <h4 className=' text-lg'>{project.title}</h4>
+                    <p className='text-gray-500 text-lg'>{project.description}</p>
+                    <button
+                      type='button'
+                      onClick={() => removeProject(index)}
+                      className=' text-sm mt-1 text-center cursor-pointer'>
+                      <FaTrash color='red' />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )}
+
+          </div>
+
+        </div>
+
 
       </div>
 
